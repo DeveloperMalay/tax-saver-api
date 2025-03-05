87ea7e955c1cb7a5c6e7b4cc4ccfdc3239cb27f0
Malay Pandit <malay13pandit@gmail.com>
2025-03-05 22:58:40 +0530

    modified user model

--- app/models/user.ts
+++ app/models/user.ts
@@ -1,9 +1,11 @@
 import { DateTime } from 'luxon'
 import hash from '@adonisjs/core/services/hash'
 import { compose } from '@adonisjs/core/helpers'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, beforeCreate, column } from '@adonisjs/lucid/orm'
 import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
 import { DbAccessTokensProvider } from '@adonisjs/auth/access_tokens'
+import { v4 as uuidv4 } from 'uuid'
+import mail from '@adonisjs/mail/services/main'
 
 const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
   uids: ['email'],
@@ -12,7 +14,7 @@ const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
 
 export default class User extends compose(BaseModel, AuthFinder) {
   @column({ isPrimary: true })
-  declare id: number
+  declare id: string
 
   @column()
   declare fullName: string | null
@@ -29,5 +31,39 @@ export default class User extends compose(BaseModel, AuthFinder) {
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime | null
 
-  static accessTokens = DbAccessTokensProvider.forModel(User)
+  @column()
+  declare accountStatus: string
+
+  @column()
+  declare isVerified: boolean
+
+  @column()
+  declare verificationToken?: string
+
+  static accessTokens = DbAccessTokensProvider.forModel(User, {
+    expiresIn: '30 days',
+    prefix: 'oat_',
+    table: 'auth_access_tokens',
+    type: 'auth_token',
+    tokenSecretLength: 40,
+  })
+
+  @beforeCreate()
+  public static assignUuid(user: User) {
+    user.id = uuidv4()
+  }
+
+  @beforeCreate()
+  public static async assignVerificationToken(user: User) {
+    user.verificationToken = uuidv4()
+  }
+
+  public async sendVerificationEmail() {
+    await mail.send((message) => {
+      message
+        .to(this.email)
+        .subject('Verify Your Email')
+        .htmlView('emails/verify_email', { token: this.verificationToken })
+    })
+  }
 }
