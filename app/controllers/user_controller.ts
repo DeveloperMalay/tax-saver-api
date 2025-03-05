16b55cacd06abbf6151934458f68851bc63978df
Malay Pandit <malay13pandit@gmail.com>
2025-03-05 23:53:27 +0530

    added user controller

---
+++ app/controllers/user_controller.ts
@@ -0,0 +1,22 @@
+import User from '#models/user'
+import { UserService } from '#services/users_service'
+import { loginValidator, registerValidator } from '#validators/user'
+import { inject } from '@adonisjs/core'
+import { HttpContext } from '@adonisjs/core/http'
+
+@inject()
+export default class UserController {
+  constructor(protected authService: UserService) {}
+
+  public async register({ request, response }: HttpContext) {
+    const payload = await registerValidator.validate(request.all())
+    const result = await this.authService.register(payload)
+    return response.ok(result)
+  }
+
+  public async login({ request, response }: HttpContext) {
+    const payload = await loginValidator.validate(request.all())
+    const user = await this.authService.login(payload)
+    return response.ok(user)
+  }
+}
--- app/validators/user.ts
+++ app/validators/user.ts
@@ -1,9 +1,16 @@
 import vine from '@vinejs/vine'
 
-export const UserValidator = vine.compile(
+export const registerValidator = vine.compile(
   vine.object({
     username: vine.string().trim().maxLength(255),
     email: vine.string().trim().email(),
     password: vine.string().trim().minLength(8),
+  })
+)
+
+export const loginValidator = vine.compile(
+  vine.object({
+    email: vine.string().email().trim().normalizeEmail(),
+    password: vine.string().minLength(8).maxLength(32),
   })
 )
